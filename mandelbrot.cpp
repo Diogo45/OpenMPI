971a32468ebe0064dbb101edc0d33e902926ef8e
Diogo45 <diogohartmann45@gmail.com>
2020-05-23 19:17:41 -0300

    Number of processes corerection

--- mandelbrot.cpp
+++ mandelbrot.cpp
@@ -165,15 +165,21 @@ int main(int argc, char** argv)
 		for (i=1; i < proc_n; i++)
 		{
 			source = i;
+			printf("Receiving results from task %d\n",source);
+
+
 			MPI_Recv(&offset, 1, MPI_INT, source, mtype, MPI_COMM_WORLD, &status);
+
+			
 			MPI_Recv(&rows, 1, MPI_INT, source, mtype, MPI_COMM_WORLD, &status);
-			MPI_Recv(&r[offset][0], rows*n, MPI_INT, source, mtype, 
-					MPI_COMM_WORLD, &status);
-			MPI_Recv(&g[offset][0], rows*n, MPI_INT, source, mtype, 
-					MPI_COMM_WORLD, &status);
-			MPI_Recv(&b[offset][0], rows*n, MPI_INT, source, mtype, 
-					MPI_COMM_WORLD, &status);
-			printf("Received results from task %d\n",source);
+
+			printf("Received offset and rows from task %d\n",source);
+			MPI_Recv(&r[offset][0], rows*n, MPI_INT, source, mtype, MPI_COMM_WORLD, &status);
+			printf("Received r from task %d\n",source);
+			MPI_Recv(&g[offset][0], rows*n, MPI_INT, source, mtype, MPI_COMM_WORLD, &status);
+			printf("Received g from task %d\n",source);
+			MPI_Recv(&b[offset][0], rows*n, MPI_INT, source, mtype, MPI_COMM_WORLD, &status);
+			printf("Received b from task %d\n",source);
 		}
 
 
