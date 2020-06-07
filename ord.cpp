4d8278b8d733c806a8ff420662b8a51ac3401fef
Diogo45 <diogohartmann45@gmail.com>
2020-06-07 15:56:50 -0300

    Initial Test sort

--- ord.cpp
+++ ord.cpp
@@ -6,7 +6,7 @@
 # include <ctime>
 # include <cstring>
 #include "mpi.h"
-#include "RandomNumerGenerator.h"
+#include "RandomNumberGenerator.h"
 #define MASTER 0               /* taskid of first task */
 #define FROM_MASTER 1          /* setting a message type */
 #define FROM_WORKER 2          /* setting a message type */
