62cf395ac5be42f61c243ef4b1b7021b8769f4d1
GabrielWetzel <gabriel.rockenbach@acad.pucrs.br>
2020-06-20 17:15:21 -0300

    implementando fases paralelas

---
+++ fases_paralelas.cpp
@@ -0,0 +1,129 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "mpi.h"
+
+#define DEBUG 1            // comentar esta linha quando for medir tempo
+#define ARRAY_SIZE 40      // trabalho final com o valores 10.000, 100.000, 1.000.000
+#define PARTE 1
+
+void bs(int n, int * vetor)
+{
+    int c=0, d, troca, trocou =1;
+
+    while (c < (n-1) & trocou )
+        {
+        trocou = 0;
+        for (d = 0 ; d < n - c - 1; d++)
+            if (vetor[d] > vetor[d+1])
+                {
+                troca      = vetor[d];
+                vetor[d]   = vetor[d+1];
+                vetor[d+1] = troca;
+                trocou = 1;
+                }
+        c++;
+        }
+}
+
+int main()
+{
+    int vetor[ARRAY_SIZE];
+    
+    bool pronto = false;
+    
+    MPI_Init(&argc,&argv);
+	MPI_Comm_rank(MPI_COMM_WORLD,&my_rank);
+	MPI_Comm_size(MPI_COMM_WORLD,&proc_n);
+
+    for(int i = ARRAY_SIZE; i >= 0; i--) vetor[i] = i;
+        
+    while(!pronto)
+    {
+        bool estado[proc_n];
+        for(int i = 0; i<proc_n,i++)
+        {
+            estado[i] = false;
+        }
+        int tam = ARRAY_SIZE/proc_n;
+        int resto = ARRAY_SIZE%proc_n;
+        int menor_elem;
+        int tam_aux = tam;
+        if(my_rank == proc_n-1)
+        {
+            tam_aux+=resto;
+        }
+
+        bs(tam_aux, vetor[my_rank*tam])
+        
+        if(my_rank != proc_n-1)
+        {
+            MPI_Send(&vetor[my_rank*tam + tam_aux - 1], 1, MPI_INT, my_rank + 1, 1, MPI_COMM_WORLD);
+        }
+
+        if(my_rank != 0)
+        {
+            MPI_Recv(&menor_elem, 1, MPI_INT, my_rank - 1, 1, MPI_COMM_WORLD);
+            if(menor_elem > vetor[my_rank*tam + tam_aux - 1])
+            {
+                estados[my_rank] = true;
+            }
+        }
+        
+
+        
+       
+        pronto = true;
+        for(int i = 1; i <proc_n; i++)
+        {
+            MPI_Bcast(&estado[i], 1, MPI_BOOL, i, MPI_COMM_WORLD);
+            if(!estado[i])
+            {
+                pronto = false;
+                break;
+            }
+        }
+        
+        if(pronto) break;
+
+
+        if(my_rank != 0)
+        {
+            MPI_Send(&vetor[my_rank*tam], PARTE, MPI_INT, my_rank - 1, 1, MPI_COMM_WORLD);
+        }
+
+        vetor_aux[PARTE * 2];
+
+        if(my_rank != proc_n)
+        {
+
+
+            MPI_Recv(&vetor_aux[0], PARTE, MPI_INT, my_rank + 1, 1, MPI_COMM_WORLD);
+
+
+            bs(PARTE*2, vetor_aux);
+
+            MPI_Send(&vetor_aux[PARTE], PARTE, MPI_INT, my_rank + 1, 1, MPI_COMM_WORLD);
+
+            for(int i = 0; i< PARTE;i++)
+            {
+                vetor[my_rank*tam + i] = vetor_aux[i];
+            }
+
+         //   1 2 4 5 || 3 6 7 8
+         //   1 2 | 4 5 3 6 | 7 8
+         //   1 2 | 3 4 5 6 | 7 8
+         //   1 2 3 4 || 5 6 7 8
+        }
+
+        if(my_rank != 0){
+
+            MPI_Recv(&vetor[my_rank*tam], PARTE, MPI_INT, my_rank - 1, 1, MPI_COMM_WORLD);
+
+        }
+    }
+
+
+    MPI_Finalize();
+    
+    return 0;
+}
--- ord.cpp
+++ ord.cpp
@@ -20,40 +20,32 @@ double vec[VEC_SIZE];
 
 int main(int argc, char** argv)
 {
-    //printf("INICIANDO\n");
 
 	int my_rank;       // Identificador deste processo
 
-	int proc_n; 
+	int proc_n;        // numero total de processos
 
-    int size = VEC_SIZE; 
+    int size = VEC_SIZE; // tamanho atual do vetor
 
-    unsigned long seed = 1;
 
     double t1,t2;
 	MPI_Status status;
 
 
-    //printf("Iniciando MPI INIT\n");
 	MPI_Init(&argc,&argv);
 	MPI_Comm_rank(MPI_COMM_WORLD,&my_rank);
 	MPI_Comm_size(MPI_COMM_WORLD,&proc_n);
     
-    //printf("Terminou MPI INIT\n");
-    
-    //printf("Inicializou vetor\n");
 
     if(my_rank != MASTER)
 	{
-        //printf("Started Receiving size %d\n", my_rank);
+
+        //Se este processo nao eh o mestre, recebe de seu pai (my_rank - 1) / 2 o vetor a ser processado ou novamente dividido e o tamanho deste vetor
 
         MPI_Recv(&size, 1, MPI_INT, (my_rank - 1) / 2, 1, MPI_COMM_WORLD, &status);
-        //printf("Received size %d\n", my_rank);
-        //printf("Started Receiving vec %d\n", my_rank);
-        MPI_Recv(&vec[0], size, MPI_DOUBLE, (my_rank - 1) / 2, 1, MPI_COMM_WORLD, &status);
-        //printf("Received vec %d\n", my_rank);
 
-        //MPI_Recv(&offset, 1, MPI_INT, MASTER, 1, MPI_COMM_WORLD, &status);
+        MPI_Recv(&vec[0], size, MPI_DOUBLE, (my_rank - 1) / 2, 1, MPI_COMM_WORLD, &status);
+
     }
     else
     {
@@ -63,17 +55,15 @@ int main(int argc, char** argv)
 
         double start = MPI_Wtime();
 
-		/* Send matrix data to the worker tasks */
-		
-        //RandomNumberGenerator* rnd = new RandomNumberGenerator(seed);
-
         //CRIA VETOR RANDOM
         for(int i = 0; i < VEC_SIZE; i++)
         {
             vec[i] = (double)rand();//rnd->GetRandom(0.0, 1000000.0);
         }
+
         printf("Finalizou inicialização do vetor pelo mestre\n");
-        //for(int i = 0; i < size; i++) { std::cout << vec[i] << ", "; }
+
+        //Se este for o unico processo alocado, se faz a ordenacao total do vetor
         if(proc_n == 1)
         {
             printf("Mestre ordenando vetor sozinho\n");
@@ -92,43 +82,30 @@ int main(int argc, char** argv)
 
         printf("Chegou na folha em %d\n",my_rank);
         sort(&vec[0], size);
-        //printf("Vetor ordenado por %d\n",my_rank);
-        //for(int i = 0; i < size; i++) { std::cout << vec[i] << ", "; }
-
-        //printf("Proc %d tendando mandar vetor ordenado para proc %d \n",my_rank,(my_rank - 1) / 2 );
+        
         MPI_Send(&vec[0], size, MPI_DOUBLE, (my_rank - 1) / 2 , 1, MPI_COMM_WORLD);
-        //printf("Proc %d MANDOU vetor ordenado para proc %d \n",my_rank,(my_rank - 1) / 2 );
         MPI_Finalize();
         return 0;
        
     }
     else
     {
-        //printf("Processo %d iniciou (size = %d, VEC_SIZE/((proc_n+1)/2 = %d)\n", my_rank,size,delta);
         int newSize = size/2;
         int newSize2 = newSize + size%2;
         
 		MPI_Send(&newSize, 1, MPI_INT, my_rank * 2 + 1, 1, MPI_COMM_WORLD);
         MPI_Send(&vec[0], newSize, MPI_DOUBLE, my_rank * 2 + 1, 1, MPI_COMM_WORLD);
-        //printf("Processo %d mandou metade do vetor para processo %d\n", my_rank,my_rank * 2 + 1);
+
 		MPI_Send(&newSize2, 1, MPI_INT, my_rank * 2 + 2, 1, MPI_COMM_WORLD);
         MPI_Send(&vec[newSize], newSize2, MPI_DOUBLE, my_rank * 2 + 2, 1, MPI_COMM_WORLD);
-        //printf("Processo %d mandou metade do vetor para processo %d\n", my_rank,my_rank * 2 + 2);
 
         MPI_Recv(&vec[0], newSize, MPI_DOUBLE, my_rank * 2 + 1, 1, MPI_COMM_WORLD, &status);
-        //printf("Processo %d RECEBEU metade do vetor para processo %d\n", my_rank,my_rank * 2 + 1);
         MPI_Recv(&vec[newSize], newSize2, MPI_DOUBLE, my_rank * 2 + 2, 1, MPI_COMM_WORLD, &status);
-        //printf("Processo %d RECEBEU metade do vetor para processo %d\n", my_rank,my_rank * 2 + 2);
         
 
-        //printf("Vetor antes da intercalaçao por %d\n",my_rank);
-        //for(int i = 0; i < size; i++) { std::cout << vec[i] << ", "; }
-        //printf("\n");
-        intercala(&vec[0], size);
-        //printf("Processo %d intercalou vetores : \n", my_rank);
-        //for(int i = 0; i < size; i++) { std::cout << vec[i] << ", "; }
 
-        //printf("\n");
+        intercala(&vec[0], size);
+
 
     }
 
@@ -138,7 +115,6 @@ int main(int argc, char** argv)
         printf("\nTempo de execucao: %f\n\n", t2-t1);   
 
 
-        //for(int i = 0; i < size; i++) { std::cout << vec[i] << ", "; }
 
 
     }else
@@ -185,11 +161,6 @@ void intercala(double* vet, int size)
     }
     
 
-    //printf("VET AUX: \n");
-    //for(int i = 0; i < size; i++) { std::cout << aux[i] << ", "; }
-    //printf("\n");
-
-    //vet = &aux[0];
 
     for(int i = 0; i < size; i++) { vet[i] = aux[i]; }
 
