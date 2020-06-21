c45267ab879aa45ad0f0beaaa83be1c4916f7bf5
GabrielWetzel <gabriel.rockenbach@acad.pucrs.br>
2020-06-21 17:20:30 -0300

    implementando fases paralelas

--- fases_paralelas.cpp
+++ fases_paralelas.cpp
@@ -3,8 +3,8 @@
 #include "mpi.h"
 
 #define DEBUG 1            // comentar esta linha quando for medir tempo
-#define ARRAY_SIZE 1000      // trabalho final com o valores 10.000, 100.000, 1.000.000
-#define PARTE 10
+#define ARRAY_SIZE 10000      // trabalho final com o valores 10.000, 100.000, 1.000.000
+#define PARTE 50
 
 void bs(int n, int * vetor)
 {
