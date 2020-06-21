0189982db967367afac6644b752004bee2feb8ef
GabrielWetzel <gabriel.rockenbach@acad.pucrs.br>
2020-06-21 16:52:45 -0300

    implementando fases paralelas

--- fases_paralelas.cpp
+++ fases_paralelas.cpp
@@ -4,7 +4,7 @@
 
 #define DEBUG 1            // comentar esta linha quando for medir tempo
 #define ARRAY_SIZE 40      // trabalho final com o valores 10.000, 100.000, 1.000.000
-#define PARTE 1
+#define PARTE 2
 
 void bs(int n, int * vetor)
 {
