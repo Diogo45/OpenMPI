a4e934f959a8e58f0225283ee50a2d390036a56c
GabrielWetzel <gabriel.rockenbach@acad.pucrs.br>
2020-06-27 09:54:16 -0300

    implementando fases paralelas

--- mandelbrot.cpp
+++ mandelbrot.cpp
@@ -6,6 +6,7 @@
 # include <ctime>
 # include <cstring>
 # include "mpi.h"
+# include <omp.h>
 
 #define MASTER 0               /* taskid of first task */
 #define FROM_MASTER 1          /* setting a message type */
